c9038322f297aef49c4fcc38b0044d4b50979e21
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-03-22 15:29:11 -0700

    Create Selection_Sort.h

---
+++ Solution--HyrekanDragon/Assignment-04/Selection_Sort.h
@@ -0,0 +1,22 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* The Functions used in Selection Sort file
+*/
+
+#ifndef SELECTION_SORT_H
+#define SELECTION_SORT_H
+
+#include<vector>
+
+void selection_sort(std::vector<int> &v);
+
+void swap(int & x, int & y);
+
+void print_vector(const std::vector<int> v);
+
+#endif
+
