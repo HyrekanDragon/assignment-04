1a79386378569aea448c6b8946aab027d7e194d6
HyrekanDragon <HyrekanDragon@yahoo.com>
2016-03-22 15:25:52 -0700

    Create Merge_Sort.h

---
+++ Solution--HyrekanDragon/Assignment-04/Merge_Sort.h
@@ -0,0 +1,56 @@
+/* ----------------------------------------------------------------------------
+* Copyright &copy; 2016 Alex Chuoy <HyrekanDragon@yahoo.com>
+* Released under the [MIT License] (http://opensource.org/licenses/MIT)
+* ------------------------------------------------------------------------- */
+
+/**
+* A program merge sort the contents of a vector
+*/
+
+#include "Selection_Sort.h"
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+void print_vector(const vector<int> v)
+{
+	cout << "Printing out contents of the array.\n\n";
+
+	for (int e:v)
+	{
+		cout << e << " ";
+	}
+
+	cout << endl << endl;
+}
+
+void swap(int & x, int & y)
+{
+	int temp = x;
+	x = y;
+	y = temp;
+}
+
+void selection_sort(vector<int> & v)
+{
+	cout << "Sorting out the contents of the array.\n\n";
+
+	for (int i = 0; i < v.size(); i++)
+	{
+		int min = v[i];
+		int min_index = i;
+
+		for (int j = i + 1; j < v.size() - 1; j++)
+		{
+			if (v[j] < min)
+			{
+				min = v[j];
+				min_index = j;
+			}
+		}
+		swap(v[min_index], v[i]);
+	}
+}
